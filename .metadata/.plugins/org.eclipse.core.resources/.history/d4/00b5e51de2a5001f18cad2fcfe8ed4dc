1c06778837e26324ef8676b8502e1b3e03f1459a
User <raquelmorabito@hotmail.com>
2024-11-18 16:28:17 -0300

    Novas funcionalidades

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d4/00b5e51de2a5001f18cad2fcfe8ed4dc
@@ -0,0 +1,5 @@
+package ControleAmostrasLab.util;
+
+public class cores {
+
+}
--- ControleAmostrasLab/src/ControleAmostrasLab/util/cores.java
+++ ControleAmostrasLab/src/ControleAmostrasLab/util/cores.java
@@ -2,4 +2,27 @@ package ControleAmostrasLab.util;
 
 public class cores {
 
+    // Cor para o fundo do menu (um tom suave de azul)
+    public static final String AZUL = "\033[0;34m";
+
+    // Cor para títulos ou mensagens de destaque (um verde agradável)
+    public static final String VERDE = "\033[0;32m";
+
+    // Cor para mensagens de erro (vermelho)
+    public static final String VERMELHO = "\033[0;31m";
+
+    // Cor para texto normal (preto)
+    public static final String PRETO = "\033[0;30m";
+
+    // Cor para texto em branco
+    public static final String BRANCO = "\033[0;37m";
+
+    // Cor para texto de aviso (amarelo)
+    public static final String AMARELO = "\033[0;33m";
+
+    // Cor para o fundo (branco para contraste)
+    public static final String FUNDO_BRANCO = "\033[47m";
+
+    // Resetando as cores para o padrão do terminal
+    public static final String RESETAR = "\033[0m";
 }
